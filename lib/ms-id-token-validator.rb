8eda323921516525d7ebb41dc0aa8e39bb7c68cb
piyush-cronycle <piyush@cronycle.com>
2022-04-14 15:37:28 +0530

    validator modified

--- lib/ms-id-token-validator.rb
+++ lib/ms-id-token-validator.rb
@@ -1,5 +1,6 @@
-require "net/http"
-require "json/jwt"
+require "net/http/persistent"
+require "jwt"
+require "active_support/all"
 
 module MsIdToken
   class BadIdTokenFormat < StandardError; end
@@ -28,6 +29,12 @@ module MsIdToken
     TOKEN_TYPE = "JWT".freeze
     TOKEN_ALGORITHM = "RS256".freeze
 
+    def test 
+      id_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6ImJXOFpjTWpCQ25KWlMtaWJYNVVRRE5TdHZ4NCJ9.eyJ2ZXIiOiIyLjAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vOTE4ODA0MGQtNmM2Ny00YzViLWIxMTItMzZhMzA0YjY2ZGFkL3YyLjAiLCJzdWIiOiJBQUFBQUFBQUFBQUFBQUFBQUFBQUFJdUlBQk5TNHhFaEppOVlUczhobW5rIiwiYXVkIjoiNTIyZmEyMTQtNGQ2OC00ODRjLWJiZWYtMjFjZGI0ZmFlZDZhIiwiZXhwIjoxNjQ5ODQyNDQ0LCJpYXQiOjE2NDk3NTU3NDQsIm5iZiI6MTY0OTc1NTc0NCwibmFtZSI6IlBpeXVzaCBDcm9ueWNsZSIsInByZWZlcnJlZF91c2VybmFtZSI6InBpeXVzaEBjcm9ueWNsZS5jb20iLCJvaWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtYmJlOS1lYTU0YmFkYzA4MGUiLCJlbWFpbCI6InBpeXVzaEBjcm9ueWNsZS5jb20iLCJ0aWQiOiI5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJub25jZSI6IjY3ODkxMCIsImFpbyI6IkRSUip4UVFCRHZxSVdUSWlQcVpFeHh4Nmc3Q2t1YjVlODE0U2VVSFBLQzUxdWxrV1R4TzdjcFRYRSF6alhNQW13Qkk3UkdiaTUxV29JMW04eU1qR0NDeGdtQklMY0JxQXlpSGQzWXhVZEZ2Kkd3Q0FxWnRJSnNGeGRndzJPM3k3ZWNpdW0hNEVrKkl5NVRNUTRtYnA1UzAkIn0.UDMb0MKF6HUqD_7BFccFtaIF_iZRqgTZMHIHEpSnL6hb8LC5_gkseVUhfkkGVMqv-tixtyVBg1BSa-dwThxod7KDMCMJVwr2AhJ50_fNO_pX2N3Oj3va6AobvVohQMfUSWXpAXV0Lm8vnSE8eVdNaQweoEuJkxkWzc5FrcYatL7UqFAWooGYBfVEiDWpY15pq8MWUmnGPgpbhLQqVbV2MWre9zyDPeHQE6cLomaS9bEz4b2r23M3DHknO9wXgYO42dCUCdQardYPdw8qhsONlbKmvRAlmq1z4aEs-5FY19iEGOafO9bS0wIITqnoSg2Ymn65WUCont9o_z6cG9d6DQ"
+      audience = "522fa214-4d68-484c-bbef-21cdb4faed6a"
+      check(id_token, audience)
+    end
+
     def initialize(options = {})
       @cached_certs_expiry = options.fetch(:expiry, CACHED_CERTS_EXPIRY)
     end
@@ -37,12 +44,16 @@ module MsIdToken
 
       raise BadIdTokenFormat if encoded_payload.nil? || signature.nil?
 
-      header = JSON.parse(Base64.decode64(encoded_header), symbolize_names: true)
-
-      public_keys = JSON::JWK::Set.new(ms_public_keys)
-
-      payload = JSON::JWT.decode(id_token, public_keys).symbolize_keys
-
+      # header = JSON.parse(Base64.decode64(encoded_header), symbolize_names: true)
+      
+      # signature verification with microsoft public keys
+      jwks = ms_public_keys
+      decoded_token = JWT.decode(id_token, nil, true, { algorithms: [TOKEN_ALGORITHM], jwks: jwks})
+      
+      # validate header and payload
+      header = decoded_token[1].deep_symbolize_keys
+      verify_header(header)
+      payload = decoded_token[0].deep_symbolize_keys
       verify_payload(payload, audience)
 
       payload
@@ -53,7 +64,9 @@ module MsIdToken
     def verify_header(header)
       valid_header = header[:typ] == TOKEN_TYPE && header[:alg] == TOKEN_ALGORITHM
 
-      valid_header &= !(header[:kid].nil? && header[:x5t].nil?)
+      # "x5t" claim is for version 1.0 only
+      # valid_header &= !(header["kid"].nil? && header["x5t"].nil?)
+      valid_header &= !(header[:kid].nil?)
 
       raise BadIdTokenHeaderFormat unless valid_header
     end
--- ms-id-token-validator.gemspec
+++ ms-id-token-validator.gemspec
@@ -29,7 +29,10 @@ Gem::Specification.new do |spec|
   spec.executables = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_runtime_dependency("json-jwt", "~> 1.7")
+  spec.add_runtime_dependency("jwt")
+  spec.add_runtime_dependency("net-http-persistent")
+  spec.add_runtime_dependency("activesupport")
+
 
   spec.add_development_dependency "bundler", "~> 2.2.33"
   spec.add_development_dependency "rake", ">= 12.3.3"
